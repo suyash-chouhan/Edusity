3228340d3942674850367e5ddd6c9a7a6e0697b0
suyash chouhan <suyashchouhan03@gmail.com>
2025-08-23 22:02:23 +0530

    Initial commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.css'
+import { Link } from "react-scroll";
 import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
@@ -8,7 +9,8 @@ const Hero = () => {
       <div className="hero-text">
         <h1>We ensure better education for a better world</h1>
         <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
-        <button className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
+        <Link to='about' smooth={true} offset={-150} duration={500} className='btn'>Explore More <img src={dark_arrow} alt="" /></Link> 
+        
       </div>
     </div>
   )
